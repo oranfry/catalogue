a11152f8b3943c784fed3706f880619b0be26364
Oran Fry <oran@emerg.co>
2020-04-13 14:29:51 +1200

    Catalogue package

---
+++ src/php/class/blend/catalogue.php
@@ -0,0 +1,33 @@
+<?php
+namespace blend;
+
+class catalogue extends \Blend
+{
+    public $label = 'Catalogue';
+    public $printable = true;
+    public $linetypes = ['catalogueitem'];
+
+    public function __construct()
+    {
+        $this->fields = [
+            (object) [
+                'name' => 'icon',
+                'type' => 'icon',
+            ],
+            (object) [
+                'name' => 'sku',
+                'type' => 'text',
+            ],
+            (object) [
+                'name' => 'title',
+                'type' => 'text',
+                'sacrifice' => true,
+            ],
+            (object) [
+                'name' => 'price',
+                'type' => 'number',
+                'sacrifice' => true,
+            ],
+        ];
+    }
+}
---
+++ src/php/class/linetype/catalogueitem.php
@@ -0,0 +1,49 @@
+<?php
+namespace linetype;
+
+class catalogueitem extends \linetype\skumeta
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->label = 'tag';
+        $this->label = 'Catalogue Item';
+        $this->clause = 'catalogue.price is not null';
+        $this->inlinelinks = [
+            (object)[
+                'tablelink' => 'skumetacatalogue',
+                'linetype' => 'skumeta',
+                'alias' => 'catalogue',
+            ],
+        ];
+        $this->fields[] = (object) [
+            "name" => "price",
+            "type" => "number",
+            "dp" => 2,
+            'fuse' => 'catalogue.price',
+            'hide' => true,
+        ];
+        $this->unfuse_fields['catalogue.price'] = ':price';
+    }
+
+    public function validate($line)
+    {
+        $errors = [];
+
+        if ($line->sku == null) {
+            $errors[] = 'no sku';
+        }
+
+        if ($line->price == null) {
+            $errors[] = 'no price';
+        }
+
+        return $errors;
+    }
+
+    public function has($line, $assoc)
+    {
+        return $assoc == 'catalogue';
+    }
+}
