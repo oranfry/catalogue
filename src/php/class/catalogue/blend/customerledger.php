920b3fff3e309ae8bcfca00b459624c3d1b7131d
Oran Fry <oran@emerg.co>
2020-08-29 13:10:56 +1200

    New linetype and blend for customers

---
+++ src/php/class/catalogue/blend/customerledger.php
@@ -0,0 +1,34 @@
+<?php
+namespace catalogue\blend;
+
+class customerledger extends \Blend
+{
+    public function __construct()
+    {
+        $this->label = 'Customer Ledger';
+        $this->linetypes = ['customertransaction'];
+        $this->groupby = 'date';
+        $this->fields = [
+            (object) [
+                'name' => 'icon',
+                'type' => 'icon',
+                'derived' => true,
+            ],
+            (object) [
+                'name' => 'date',
+                'type' => 'date',
+                'groupable' => true,
+                'main' => true,
+            ],
+            (object) [
+                'name' => 'amount',
+                'type' => 'number',
+                'summary' => 'sum',
+                'dp' => 2,
+            ],
+        ];
+        $this->past = true;
+        $this->cum = true;
+        $this->showass = ['list', 'calendar', 'graph', 'summaries'];
+    }
+}
---
+++ src/php/class/catalogue/linetype/customertransaction.php
@@ -0,0 +1,78 @@
+<?php
+namespace catalogue\linetype;
+
+class customertransaction extends \Linetype
+{
+    public function __construct()
+    {
+        $this->label = 'Customer Transaction';
+        $this->icon = 'dollar';
+        $this->table = 'customertransaction';
+        $this->summaries = null;
+        $this->showass = ['list', 'calendar', 'graph'];
+        $this->fields = [
+            (object) [
+                'name' => 'icon',
+                'type' => 'icon',
+                'fuse' => "'dollar'",
+                'derived' => true,
+            ],
+            (object) [
+                'name' => 'date',
+                'type' => 'date',
+                'groupable' => true,
+                'fuse' => '{t}.date',
+            ],
+            (object) [
+                'name' => 'amount',
+                'type' => 'number',
+                'dp' => 2,
+                'summary' => 'sum',
+                'fuse' => '{t}.amount',
+            ],
+            (object) [
+                'name' => 'tax',
+                'type' => 'number',
+                'dp' => 2,
+                'summary' => 'sum',
+                'fuse' => '{t}.tax',
+            ],
+            (object) [
+                'name' => 'net',
+                'type' => 'number',
+                'dp' => 2,
+                'summary' => 'sum',
+                'fuse' => 'ifnull({t}.amount, 0) - ifnull({t}.tax, 0)',
+            ],
+        ];
+        $this->unfuse_fields = [
+            '{t}.date' => ':{t}_date',
+            '{t}.amount' => ':{t}_amount',
+            '{t}.tax' => ':{t}_tax',
+        ];
+    }
+
+    public function get_suggested_values()
+    {
+        $suggested_values = [];
+
+        $suggested_values['account'] = get_values('transaction', 'account');
+
+        return $suggested_values;
+    }
+
+    public function validate($line)
+    {
+        $errors = [];
+
+        if ($line->date == null) {
+            $errors[] = 'no date';
+        }
+
+        if ($line->amount == null) {
+            $errors[] = 'no amount';
+        }
+
+        return $errors;
+    }
+}
