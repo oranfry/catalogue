74c538835b2fa67bc509a13877d2af530a509ec8
Oran Fry <oran@emerg.co>
2023-04-30 13:27:01 +1200

    New Jars
    Convert catalogue item linetype to new jars.

--- classes/linetype/catalogueitem.php
+++ classes/linetype/catalogueitem.php
@@ -1,72 +1,32 @@
 <?php
+
 namespace catalogue\linetype;
 
-class catalogueitem extends \Linetype
+class catalogueitem extends \jars\Linetype
 {
     public function __construct()
     {
-        $this->label = 'tag';
-        $this->label = 'Catalogue Item';
         $this->table = 'catalogueitem';
-        $this->fields = [
-            (object) [
-                'name' => 'sku',
-                'type' => 'text',
-                'fuse' => '{t}.sku',
-            ],
-            (object) [
-                'name' => 'title',
-                'type' => 'text',
-                'fuse' => '{t}.title',
-                'sacrifice' => true,
-            ],
-            (object) [
-                "name" => "description",
-                "type" => "text",
-                'fuse' => '{t}.description',
-                'hide' => true,
-            ],
-            (object) [
-                "name" => "price",
-                "type" => "number",
-                "dp" => 2,
-                'fuse' => '{t}.price',
-                'hide' => true,
-            ],
-        ];
-        $this->unfuse_fields = [
-            '{t}.sku' => (object) [
-                'expression' => ':{t}_sku',
-                'type' => 'varchar(255)',
-            ],
-            '{t}.title' => (object) [
-                'expression' => ':{t}_title',
-                'type' => 'varchar(255)',
-            ],
-            '{t}.description' => (object) [
-                'expression' => ':{t}_description',
-                'type' => 'varchar(255)',
-            ],
-            '{t}.price' => (object) [
-                'expression' => ':{t}_price',
-                'type' => 'decimal(18, 2)',
-            ],
-        ];
+
+        $this->simple_string('sku');
+        $this->simple_string('title');
+        $this->simple_string('description');
+        $this->simple_float('price', 2);
     }
 
     public function validate($line)
     {
         $errors = [];
 
-        if ($line->sku == null) {
+        if (!@$line->sku) {
             $errors[] = 'no sku';
         }
 
-        if ($line->title == null) {
+        if (!@$line->title) {
             $errors[] = 'no title';
         }
 
-        if ($line->price == null) {
+        if (!@$line->price) {
             $errors[] = 'no price';
         }
 
